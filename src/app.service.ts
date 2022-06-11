ed7f0adf4a2bcd6f5eb49be78c56bda3a463f65e
Richard Huáman Gómez <richard21hg92@gmail.com>
2022-06-11 16:49:52 -0500

    se finalizo configuracion de servicio que recibira un evento user_create enviado desde el api-base

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserEvent } from './create-user-event';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  resolverCreaetUser(data: CreateUserEvent) {
+    console.log('handleUserCreate - COMUNICACION', data);
   }
 }
